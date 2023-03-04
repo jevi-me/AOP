d1e182e51c5e92117162b2ab06beec3ace09b71d
Jevi <42013557+jevi-me@users.noreply.github.com>
2023-03-04 13:34:15 -0500

    Update playerplayer.rb

--- Code/Version 6/playerplayer.rb
+++ Code/Version 6/playerplayer.rb
@@ -7,8 +7,8 @@
 
 
 # "AOP"
-# Uses a Akai LPD8 Mk2 Laptop Pad Controller 
-# as part of an instrument that allows both me (left channel) 
+# Uses a Akai LPD8 Mk2 Laptop Pad Controller
+# as part of an instrument that allows both me (left channel)
 # and the AOP (right channel) to perform within the ensemble.
 # Similar: https://gist.github.com/rbnpi/f995f7d9bf176e6b5d0f62baff257dc8
 
@@ -20,7 +20,7 @@
 # Pad Control Assignments
 # p5 - short sound
 # p6 - short sound
-# p7 - drone 
+# p7 - drone
 # p8 - drone
 
 # p1 - improvise
@@ -37,26 +37,21 @@
 # k5 - k[5] cutoff
 # k6 - k[6] pitch
 # k7 - k[7] density
-# k8 - k[8] volume 
+# k8 - k[8] volume
 
 
 #### INITIALISE AND DEFINE
 ## Define and Initialise values used to communicate with the live_loops
 
-# Pan Position of Player and AOP in sound environment 
-i_pos = -1 #left channel
-aop_pos = 1 #right channel
-
-
-# Store Input Data
-set :pads = [] #store pad data
-set :knobs = [] #store knob data
+# Pan Position of Player and AOP in sound environment
+set :i_pos, -1 #left channel
+set :aop_pos, 1 #right channel
 
 # Values for Pads
 set :improvise, 0 #improvise, on or off
 set :trigger, 0   #trigger, on or off
-set :aop_play, 0  #aop plays, on or off 
-set :i_play, 1    #I play, on or off
+set :i_ready, 1    #I play, on or off
+set :aop_ready, 0  #aop plays, on or off
 
 
 # Values for Knobs
@@ -70,7 +65,9 @@ set :adjvol, 1    #volume between 0 and 2
 set :adjdens, 1   #denisty between 0 and 2
 set :adjpitch, 1  #pitch between 0 and 2
 
-
+# Store Input Data
+knobs = [:adjatt, :adjdec, :adjsus, :adjrel, :adjcut, :adjvol, :adjdens, :adjpitch]
+pads = Array.new(8, '0') #initilised to amp 0
 
 
 #### CAPTURE MIDI CONTROLS
@@ -81,15 +78,15 @@ define :norm do |n| #scale 0->127 to 0->2
 end
 
 ## Capture knob change
-live_loop :con_chg do 
+live_loop :con_chg do
   use_real_time
   portid, portn, knb_no, val = sync "/midi*/control_change"
-  knobs[knb_no] = norm(val) 
+  knobs[knb_no] = norm(val)
   puts "knob #:", knb_no, "value:", val, "norm:", norm(val) #add to log
 end
 
-## Capture button change
-live_loop :note_on do 
+## Capture pad change
+live_loop :note_on do
   use_real_time
   portid, portn, pad_no, vel = sync "/midi*/note_on"
   amp = vel/127.0 # button pressure, sets play volume
@@ -98,13 +95,11 @@ live_loop :note_on do
 end
 
 
-#### PLAY
-live_loop :
-end
+#### SECTION PLAYERS
+# Play when a pad is pressed, if the player is ready.
 
+#live_loop :iplayer do
+#end
 
-
-
-
-
-
+#live_loop :aopplayer do
+#end
