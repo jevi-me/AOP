757b0917f8d4dc6fb73606457ff305355b54f171
Jevi <42013557+jevi-me@users.noreply.github.com>
2023-03-05 12:21:33 -0500

    captures everything no play

--- Code/Version 7/conductor.rb
+++ Code/Version 7/conductor.rb
@@ -11,13 +11,13 @@
 
 
 p_name=["mode3","mode4","i_play","aop+play", #pads
-        "mode1","mode2","improvise1","improveise2"]
+        "mode1","mode2","improvise1","improvise2"]
 
 k_name=["ctrl_d1","ctrl_d2","pitch", "density",
         "decay","sustain","release","volume"] #knobs
 
-c_names=["hit1", "hit2", "drone1", "drone2",
-         "improv_burst","trigger-up", "trigger-down"] #game controller
+c_name=["hit1","drone2", "hit2", "drone1",
+        "improv_burst","trigger-up", "trigger-down"] #game controller
 
 # ---------------------------------------------------------
 #### INITIALISE AND DEFINE STATES
@@ -39,7 +39,7 @@ set :improv2, 0   #on or off
 
 set :mode3, 0     #on or off
 set :mode4, 0     #on or off
-set :i_ready, 1    #I play, on or off
+set :i_ready, 0    #I play, on or off
 set :aop_ready, 0  #aop plays, on or off
 
 
@@ -52,22 +52,26 @@ set :adjdens, 0.2   #denisty between 0+0.2 and 1+ 0.2
 set :adjdec, 0    #decay between 0 and 1
 set :adjsus, 0    #sustain between 0 and 1
 set :adjrel, 0.8    #release between 0+0.8 and 1+ 0.8
-set :adjvol, 1    #volume between 0 and 1
+set :adjvol, 0    #volume between 0 and 1
 
 # ---------------------------------------------------------
 #### CAPTURE CONTROLS
 # ---------------------------------------------------------
 
 ## Function to Normalise Knob Inputs
-define :norm do |n| #scale 0->127 to 0->1
+define :normf do |n| #scale 0->127 to 0->1
   return n.to_f/127
 end
 
+define :norm do |n| #scale 0->127 to 0->1
+  return n/127
+end
+
 ## Capture knob change
-live_loop :con_chg do
+live_loop :knb_chg do
   use_real_time
   knb_no, val = sync "/midi*/control_change"
-  val = norm(val)
+  val = normf(val)
   
   if knb_no == 1 then
     set :ctrl_d1, val
@@ -107,76 +111,45 @@ live_loop :con_chg do
   end
 end
 
-## Capture Button Change
-live_loop :con_chg do
+## Capture Pad Change
+live_loop :pad_chg do
   use_real_time
   pad_no, val = sync "/midi*/note*"
+  val = norm(val)
   
   if pad_no == 5 then
-    puts p_name[4]
-    if get(:mode1) == 1 then
-      set :mode1, 0
-    elsif get(:mode1) == 0 then
-      set :mode1, 1
-    end
+    puts p_name[4], val
+    set :mode1, val
   end
   if pad_no == 6 then
     puts p_name[5]
-    if get(:mode2) == 1 then
-      set :mode2, 0
-    elsif get(:mode2) == 0 then
-      set :mode2, 1
-    end
+    set :mode2, val
   end
   if pad_no == 7 then
-    puts p_name[6]
-    if get(:improv1) == 1 then
-      set :improv1, 0
-    elsif get(:improv1) == 0 then
-      set :improv1, 1
-    end
+    puts p_name[6], val
+    set :improv1, val
   end
   
   if pad_no == 8 then
-    puts p_name[7]
-    if get(:improv2) == 1 then
-      set :improv2, 0
-    elsif get(:improv2) == 0 then
-      set :improv2, 1
-    end
+    puts p_name[7], val
+    set :improv2, val
   end
   
   if pad_no == 1 then
-    puts p_name[0]
-    if get(:mode3) == 1 then
-      set :mode3, 0
-    elsif get(:mode3) == 0 then
-      set :mode3, 1
-    end
+    puts p_name[0], val
+    set :mode3, val
   end
   if pad_no == 2 then
-    puts p_name[1]
-    if get(:mode4) == 1 then
-      set :mode4, 0
-    elsif get(:mode4) == 0 then
-      set :mode4, 1
-    end
+    puts p_name[1], val
+    set :mode4, val
   end
   if pad_no == 3 then
-    puts p_name[2]
-    if get(:i_ready) == 1 then
-      set :i_ready, 0
-    elsif get(:i_ready) == 0 then
-      set :i_ready, 1
-    end
+    puts p_name[2], val
+    set :i_ready, val
   end
   if pad_no == 4 then
-    puts p_name[3]
-    if get(:aop_ready) == 1 then
-      set :aop_ready, 0
-    elsif get(:aop_ready) == 0 then
-      set :aop_ready, 1
-    end
+    puts p_name[3], val
+    set :aop_ready, val
   end
 end
 
@@ -199,6 +172,12 @@ end
 live_loop :rud do #up/down
   use_real_time
   b= sync "/osc*/rud"
+  if b[0] > 0
+    puts c_name[0]
+  end
+  if b[0] < 0
+    puts c_name[2]
+  end
 end
 
 # Drones
@@ -207,7 +186,6 @@ live_loop :bt3 do #4
   b= sync "/osc*/b3"
   puts c_name[3]
 end
-
 
 live_loop :bt1 do #2
   use_real_time
@@ -219,22 +197,25 @@ end
 live_loop :rlr do # left/right
   use_real_time
   b= sync "/osc*/rlr"
+  if b[0] > 0
+    puts c_name[1]
+  end
+  if b[0] < 0
+    puts c_name[3]
+  end
 end
 
 # Improvise
-
 live_loop :bt9 do #Start
   use_real_time
   b= sync "/osc*/b9"
   puts c_name[4]
-  
 end
 
 live_loop :bt8 do #Select
   use_real_time
   b=sync "/osc*/b8"
   puts c_name[4]
-  
 end
 
 
@@ -243,26 +224,22 @@ live_loop :bt6 do  #Left Up
   use_real_time
   b=sync "/osc*/b6"
   puts c_name[5]
-  
 end
 
 live_loop :bt4 do #Left down
   use_real_time
   b=sync "/osc*/b4"
   puts c_name[6]
-  
 end
 
 live_loop :bt7 do  #Right Up
   use_real_time
   b=sync "/osc*/b7"
   puts c_name[5]
-  
 end
 
 live_loop :bt5 do #Right Down
   use_real_time
   b=sync "/osc*/b5"
   puts c_name[6]
-  
 end
